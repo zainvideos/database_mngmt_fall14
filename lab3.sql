16f090a6d41d6a64f6a9cc4d04bf9cfd174811a8
zainvideos <zainvideos@gmail.com>
2014-09-23 02:40:28 -0400

    lab 3 submission
    Submitted by Zain Qayyum, sql queries for lab 3.

---
+++ lab3.sql
@@ -0,0 +1,35 @@
+﻿select name,city
+from agents
+where name= 'Bond';
+
+select pid,name,quantity
+from products
+where priceUSD > .99;
+
+select ordno,qty
+from orders;
+
+select name,city
+from customers
+where city ='Duluth';
+
+select name
+from agents
+where city != 'New York' AND city != 'London'  ;
+
+select *
+from products
+where city != 'Dallas' AND city != 'Duluth' AND priceUSD < 1;
+
+select *
+from orders
+where mon = 'jan' OR mon = 'apr';
+
+select *
+from orders
+where mon = 'feb' AND dollars > 200;
+
+
+select *
+from customers
+where cid = 'c005';
