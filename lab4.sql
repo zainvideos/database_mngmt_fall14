2c67b94eb66f79e7e130728891fa100d4960e7b6
zainvideos <zainvideos@gmail.com>
2014-09-29 08:05:11 -0400

    Lab 4
    By Zain Qayyum 9/29/14, first commit

---
+++ lab4.sql
@@ -0,0 +1,29 @@
+﻿-- Zain Qayyum lab 4
+-- Question 1
+Select 
+city
+from 
+Agents
+where 
+exists (
+select 
+aid
+from 
+Orders
+where 
+cid ='c001'
+);
+-- Question 2
+Select 
+pid
+from 
+Products
+where 
+exists (
+select 
+aid
+from 
+Orders
+where 
+cid ='c006'
+);
